ddb6e9d29bc8da4c5e1749bda0a98e2378bfeef1
edwin rivera <waehake@gmail.com>
2018-01-17 17:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var recipes2 = Object.assign({}, object, { key: value });
   return recipes2;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
